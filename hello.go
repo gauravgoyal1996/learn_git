19c2c6cceec6d41725c6d9ba9aa6df959d4176c8
gauravgoyal1996 <gauravg1000@gmail.com>
2018-09-04 12:21:26 +0530

    First Commit

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+// gaurav goyal
+import "fmt"
+
+func main() {
+	var a int = 47
+	if a >= 18 {
+	fmt.Println("eligivble to vote")
+	} else {
+		fmt.Println("not eligible to vote")
+	}
+}
