d93eb5ab95a7053c85cbfc1e5725a657a761c7e7
gauravgoyal1996 <gauravg1000@gmail.com>
2018-09-11 14:13:18 +0530

    first commit

--- hello.go
+++ hello.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var a int = 47
+	var a int = 17
 	if a >= 18 {
 		//persons eligible to vote
 	fmt.Println("eligible to vote")
+	fmt.Println("please do vote")
 	} else {
 		// not yet eligible
 		fmt.Println("not eligible to vote")
