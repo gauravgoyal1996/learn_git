dd3cb92638d24c4f2b1a299dacf99896f0a4d695
gauravgoyal1996 <gauravg1000@gmail.com>
2018-09-11 15:32:23 +0530

    comments commit

--- hello.go
+++ hello.go
@@ -12,4 +12,13 @@ func main() {
 		// not yet eligible
 		fmt.Println("not eligible to vote")
 	}
+/*$ git status
+# On branch amend-my-name
+# Changes not staged for commit:
+#   (use "git add <file>..." to update what will be committed)
+#   (use "git checkout -- <file>..." to discard changes in working directory)
+#
+#   modified:   attendees_and_learners.rst
+#
+no changes added to commit (use "git add" and/or "git commit -a")*/
 }
